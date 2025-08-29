d6d50278471ffa8c42de6019eb91482663e286dc
Szymon Duchniewicz <szduchniewicz@gmail.com>
2025-08-29 16:00:37 +1000

    loader: add plat support for odroidc2
    Based on 496b1c9ca0eff287552d01f38a7cc19e258d5a4b
    
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

--- crates/sel4-kernel-loader/src/plat/mod.rs
+++ crates/sel4-kernel-loader/src/plat/mod.rs
@@ -16,6 +16,9 @@ sel4_cfg_if! {
     } else if #[sel4_cfg(all(any(ARCH_RISCV64, ARCH_RISCV32), any(PLAT_SPIKE, PLAT_QEMU_RISCV_VIRT, PLAT_HIFIVE)))] {
         #[path = "riscv_generic/mod.rs"]
         mod imp;
+    } else if #[sel4_cfg(all(any(ARCH_AARCH64, ARCH_AARCH32), PLAT_ODROIDC2))] {
+        #[path = "odroidc2/mod.rs"]
+        mod imp;
     }
 }
 
@@ -26,6 +29,9 @@ mod bcm2711;
 mod qemu_arm_virt;
 #[cfg(any())]
 mod riscv_generic;
+#[cfg(any())]
+mod odroidc2;
+// TODO: need to add meson?
 
 #[allow(unused_imports)]
 pub(crate) use imp::*;
